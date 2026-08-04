161026e151aa89efeb16c8b01da2995a03c87abb
Ravi <gangwarr132@gmail.com>
2026-08-04 23:36:35 +0530

    ladderPattern Modified

--- Lecture Code/WhileLoops Methods Arrays/src/ladderPattern.java
+++ Lecture Code/WhileLoops Methods Arrays/src/ladderPattern.java
@@ -2,30 +2,27 @@ import java.util.Scanner;
 
 public class ladderPattern {
     public static void main(String[] args) {
-        Scanner sc=new Scanner (System.in);
-        int n=sc.nextInt();
-        for(int i=1; i<=n*n; i++){
-            for(int j=1; j<=n*n; j++){
-                if(i<=n &&j<=n){
-                    System.out.print("*");}
 
-                if(i<=2*n &&j<=2*n && i>n&&j>n){
-                    System.out.print("*");
-                }
-                if(i<=3*n &&j<=3*n&&i>2*n&&j>2*n){
-                    System.out.print("*");
-                }
-                if(i<=4*n &&j<=4*n&&i>3*n&&j>3*n){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter the steps of the ladder: ");
+        int num = input.nextInt();
+
+        for (int outerLoop = 1 ; outerLoop<=num; outerLoop++){
+            for (int row = 1 ; row<=num; row++){
+
+                for(int spaces = 1 ; spaces<=(num-1)*outerLoop+1-num; spaces++){
+                    System.out.print("  ");
                 }
 
+                for (int col = 1; col<=num; col++){
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
+
+
 
 
     }
